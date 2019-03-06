7cdd1d9508629206d7994c2782590c71fad0b4f8
Brian <bc06255@georgiasouthern.edu>
2019-03-06 12:27:51 -0500

    First commit

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp1
 {
-    partial class Form1
+    partial class userSelectWindow
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.employeebtn = new System.Windows.Forms.Button();
+            this.customerbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // employeebtn
+            // 
+            this.employeebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold);
+            this.employeebtn.Location = new System.Drawing.Point(104, 171);
+            this.employeebtn.Name = "employeebtn";
+            this.employeebtn.Size = new System.Drawing.Size(220, 70);
+            this.employeebtn.TabIndex = 0;
+            this.employeebtn.Text = "Employee";
+            this.employeebtn.UseVisualStyleBackColor = true;
+            // 
+            // customerbtn
+            // 
+            this.customerbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold);
+            this.customerbtn.Location = new System.Drawing.Point(473, 171);
+            this.customerbtn.Name = "customerbtn";
+            this.customerbtn.Size = new System.Drawing.Size(220, 70);
+            this.customerbtn.TabIndex = 1;
+            this.customerbtn.Text = "Customer";
+            this.customerbtn.UseVisualStyleBackColor = true;
+            // 
+            // userSelectWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.customerbtn);
+            this.Controls.Add(this.employeebtn);
+            this.Name = "userSelectWindow";
+            this.Text = "User Selection";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button employeebtn;
+        private System.Windows.Forms.Button customerbtn;
     }
 }
 
